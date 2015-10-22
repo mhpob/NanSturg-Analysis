e1d1bd2c50779b8fa9095b93c35d81c760abc6d6
mhpob <mikeob9@gmail.com>
2015-10-22 14:59:09 -0400

    Water quality mapping. Code adpoted from CSI analysis.

---
+++ WQmapping.R
@@ -0,0 +1,81 @@
+library(TelemetryR); library(rgdal); library(ggplot2); library(dplyr)
+
+## Input -------------------------------------------------------------------
+marnan <- readOGR('c:/users/secor lab/desktop/gis products/nanticoke2015',
+                'MarshNan')
+
+marnan <- spChFIDs(marnan, paste0(marnan@data$River, row.names(marnan)))
+
+marnan.df <- fortify(marnan)
+
+mar.plot <- filter(marnan.df, grepl('Mar', group))
+nan.plot <- filter(marnan.df, grepl('Nan', group))
+
+# Detection data
+wq.data <- read.csv('p:/obrien/biotelemetry/nanticoke/marshnan_data.csv',
+                    stringsAsFactors = F)
+wq.data$Detections <- as.factor(wq.data$Detections)
+wq.data <- wq.data %>%
+  mutate(pred.growth = sturgrow(Temp, Sal, DO.pct),
+         Date = lubridate::mdy(Date))
+
+
+ggplot() +
+  geom_polygon(data = marnan.df, aes(x = long, y = lat, group = group)) +
+  geom_point(data = wq.data, aes(x = DD.Long, y = DD.Lat), color = 'red') +
+  facet_wrap(~ Date)
+
+WQplot <- function(var, type = 'B', system = 'all'){
+  title.type <- switch(type,
+                       B = 'Bottom',
+                       S = 'Surface')
+  title.print <- switch(var,
+                  Depth = 'Depth (m)',
+                  Temp = 'Temperature (°C)',
+                  Cond = 'Conductivity (µS/cm)',
+                  Sal = 'Salinity',
+                  DO.pct = 'Dissolved Oxygen (% Saturation)',
+                  DO.mg_l = 'Dissolved Oxygen (mg/L)',
+                  pred.growth = 'Predicted Growth (per day)')
+  title.print <- paste(title.type, title.print, sep = ' ')
+
+  legend.print <- switch(var,
+                   Depth = 'Depth',
+                   Temp = 'Temperature',
+                   Cond = 'Conductivity',
+                   Sal = 'Salinity',
+                   DO.pct = 'DO (%)',
+                   DO.mg_l = 'DO (mg/L)',
+                   pred.growth = 'Growth')
+
+  system.plot <- switch(system,
+                        all = marnan.df,
+                        nan = nan.plot,
+                        mar = mar.plot)
+
+  system.dat <- switch(system,
+                   all = wq.data,
+                   nan = filter(wq.data, grepl('Nan', Site.ID)),
+                   mar = filter(wq.data, grepl('Mar', Site.ID)))
+
+  ggplot(environment = environment()) +
+  geom_point(data = filter(system.dat, Type == type),
+             aes(x = DD.Long, y = DD.Lat, color = pred.growth,
+                 size = Detections),
+                 environment = environment())+
+  facet_wrap(~ Date, ncol = 2) +
+  scale_color_continuous(low = 'blue', high = 'orange') +
+  scale_size_manual(values = c(4, 10),
+                    breaks = c('0', '1')) +
+  geom_point(data = filter(system.dat, Detections != '0'),
+             aes(x = DD.Long, y = DD.Lat, size = Detections),
+             shape = 21, color = 'black') +
+  geom_polygon(data = system.plot,
+                     aes(x = long, y = lat, group = group),
+                     fill = 'lightgray', color = 'black', alpha = 0.3) +
+  theme_bw() +
+  labs(x = 'Longitude', y = 'Latitude', title = title.print,
+       color = legend.print)
+}
+
+WQplot('pred.growth')
