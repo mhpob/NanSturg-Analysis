0a1ee6f9941486ffdaa5f09132d6743acbad1e75
mhpob <mikeob9@gmail.com>
2015-11-12 13:28:44 -0500

    Include predicted growth.

--- SondeComp.R
+++ SondeComp.R
@@ -6,9 +6,10 @@ sonde <- lapply(sonde, FUN = read.csv, stringsAsFactors = F)
 sonde <- do.call(rbind.data.frame, sonde)
 names(sonde) <- c('Date.Time', 'Temp', 'Cond', 'Sal', 'pH', 'pH.mv', 'DO.pct',
                   'DO.mg_l', 'Batt', 'Station', 'Lat', 'Long')
+sonde$pred.growth <- TelemetryR::sturgrow(sonde$Temp, sonde$Sal, sonde$DO.pct)
 test <- reshape2::melt(sonde, id.vars = c('Date.Time', 'Station'),
                        measure.vars = c('Temp', 'Cond', 'Sal', 'pH', 'DO.pct',
-                                        'DO.mg_l'))
+                                        'DO.mg_l', 'pred.growth'))
 
 test <- test %>%
   mutate(Date.Time = lubridate::mdy_hms(Date.Time),
