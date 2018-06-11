05880c62dfe024d118475e897ec0dd5f095026a0
mhpob <mikeob9@gmail.com>
2018-06-11 11:41:03 -0400

    Convert from sp to sf.

--- habitatvdetection_mapping.R
+++ habitatvdetection_mapping.R
@@ -1,20 +1,22 @@
 library(ggplot2); library(dplyr); library(sf)
 
 # Import positions ----
-all_pos <- read.csv('p:/obrien/biotelemetry/nanticoke/vps results/positions/all-calc-positions.csv',
-                stringsAsFactors = F) %>%
-  mutate(DATETIME = lubridate::ymd_hms(DATETIME))
-names(all_pos) <- tolower(names(all_pos))
+all_pos <- read.csv(
+  'p:/obrien/biotelemetry/nanticoke/vps results/positions/all-calc-positions.csv',
+  stringsAsFactors = F) %>%
+  mutate(DATETIME = lubridate::ymd_hms(DATETIME)) %>%
+  rename_all(tolower)
 
-# Select fish positions
 fish_pos <- all_pos %>%
-  filter(grepl('^\\d', all_pos$transmitter))
+  filter(grepl('^\\d', all_pos$transmitter)) %>%
+  st_as_sf(coords = c('lon', 'lat'), crs = 4326)
 
 # Import receiver positions
 rec_pos <- readxl::read_excel(
   'p:/obrien/biotelemetry/nanticoke/vps results/vps-nanticokeriver-01.xls',
   sheet = 'Stations', range = 'A3:J35') %>%
-  filter(Args == 'Derived')
+  filter(Args == 'Derived') %>%
+  st_as_sf(coords = c('Longitude', 'Latitude'), crs = 4326)
 
 # Import bottom type polygons ----
 habitat <- st_read(dsn ='C:/Users/secor/Downloads/2015 Atlantic Sturgeon Habitat Geodatabase and Report Nanticoke and Tributaries-2016-01-19/2015 Atlantic Sturgeon Habitat Geodatabase Nanticoke and Tributaries 01132016.gdb',
@@ -28,36 +30,20 @@ habitat <- habitat %>%
          SubGroup = case_when(SubGroup == '<Null>' ~ '',
                               T ~ as.character(SubGroup))) %>%
   # Reproject to lonlat
-  st_transform('+proj=longlat +zone=18 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs') %>%
+  st_transform(4326) %>%
   st_crop(xmin = -75.8145, xmax = -75.8095, ymin = 38.6415, ymax = 38.649)
 
 # Plot ----
-ggplot() +
-  geom_polygon(data = hab.df, aes(x = long, y = lat, group = group,
-                             fill = interaction(Group_, SubGroup)),
-               color = 'black') +
-  scale_fill_manual(values = c('orange1', 'orangered', 'yellow', 'lightblue', 'blue'),
-                    labels = c('Mud', 'Muddy Sand', 'Sand', 'Gravelly Sand',
-                               'Sandy Gravel')) +
-  coord_map(xlim = c(-75.8145, -75.8095), ylim = c(38.6415, 38.649)) +
-  geom_point(data = fish_pos, aes(x = lon, y = lat), alpha = 0.2) +
-  geom_point(data = rec_pos, aes(x = Longitude, y = Latitude),
-             pch = 24, lwd = 2, fill = 'green') +
-  labs(x = 'Latitude', y = 'Longitude', fill = 'Bottom Type') +
-  theme_bw() +
-  theme(legend.justification = c(0,0), legend.position = c(0.05,0.05))
-
-
 ggplot() +
   geom_sf(data = habitat, aes(fill = interaction(Group_, SubGroup)),
           color = 'black') +
   scale_fill_manual(values = c('orange1', 'orangered', 'yellow', 'lightblue', 'blue'),
                     labels = c('Mud', 'Muddy Sand', 'Sand', 'Gravelly Sand',
                                'Sandy Gravel')) +
-  coord_sf(xlim = c(-75.8145, -75.8095), ylim = c(38.6415, 38.649)) +
-  geom_point(data = fish_pos, aes(x = lon, y = lat), alpha = 0.2) +
-  geom_point(data = rec_pos, aes(x = Longitude, y = Latitude),
-             pch = 24, lwd = 2, fill = 'green') +
+
+  geom_sf(data = fish_pos, alpha = 0.2) +
+  geom_sf(data = rec_pos, pch = 24, lwd = 2, fill = 'green') +
+  coord_sf(xlim = c(-75.8145, -75.8095), ylim = c(38.6415, 38.649), expand = F) +
   labs(x = NULL, y = NULL, fill = 'Bottom Type') +
   theme_bw() +
   theme(legend.justification = c(0,0), legend.position = c(0.05,0.05))
--- spatial analysis.R
+++ spatial analysis.R
@@ -1,56 +1,41 @@
-library(dplyr); library(raster)
+library(dplyr); library(sf)
 
 # Data loading ----
 # Load NCBO bottom type data and pull out Marshyhope polygons
-habitat <- rgdal::readOGR(dsn ='C:/Users/secor/Downloads/2015 Atlantic Sturgeon Habitat Geodatabase and Report Nanticoke and Tributaries-2016-01-19/2015 Atlantic Sturgeon Habitat Geodatabase Nanticoke and Tributaries 01132016.gdb',
+habitat <- read_sf(dsn ='C:/Users/secor/Downloads/2015 Atlantic Sturgeon Habitat Geodatabase and Report Nanticoke and Tributaries-2016-01-19/2015 Atlantic Sturgeon Habitat Geodatabase Nanticoke and Tributaries 01132016.gdb',
                           layer = 'RiverBed_Habitat_Polygons_CMECS_SC_01132016')
 habitat <- habitat[habitat$Location == 'Marshyhope Creek, MD',]
 
-# Reproject from UTM to longlat
-habitat <- spTransform(habitat, CRS = CRS('+proj=longlat +zone=18 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'))
+# Reproject from UTM to longlat (epsg = 4326)
+habitat <- st_transform(habitat, 4326)
 
 
 # Load position data
 all_pos <- read.csv(
   'p:/obrien/biotelemetry/nanticoke/vps results/positions/all-calc-positions.csv',
                     stringsAsFactors = F) %>%
-  mutate(DATETIME = lubridate::ymd_hms(DATETIME))
-names(all_pos) <- tolower(names(all_pos))
+  mutate(DATETIME = lubridate::ymd_hms(DATETIME)) %>%
+  rename_all(tolower)
 
 fish_pos <- all_pos %>%
-  filter(grepl('^\\d', all_pos$transmitter))
+  filter(grepl('^\\d', all_pos$transmitter)) %>%
+  st_as_sf(coords = c('lon', 'lat'), crs = 4326)
 
 
 # Point-in-Polygon analysis ----
 # Find which habitats detections fall into
-pip <- over(SpatialPoints(coords = fish_pos[, c('lon', 'lat')],
-                          proj4string = CRS(proj4string(habitat))),
-            habitat) %>%
-  cbind(fish_pos) %>%
+pip <- st_join(fish_pos, habitat, join = st_intersects) %>%
   group_by(transmitter, Group_, SubGroup) %>%
   summarize(n())
 
 # Area of bottom types in VPS array ----
-# Create bounding box of fish detections
-bounds <-  bbox(cbind(fish_pos$lon, fish_pos$lat))
-bounds <- matrix(c(bounds[1, 1], bounds[2, 1],
-                     bounds[1, 1], bounds[2, 2],
-                     bounds[1, 2], bounds[2, 2],
-                     bounds[1, 2], bounds[2, 1],
-                     bounds[1, 1], bounds[2, 1]),
-                     ncol = 2, byrow = T)
-
-bounds <- SpatialPolygons(list(Polygons(list(Polygon(bounds)),
-                                          'Fish detection bounding box')),
-                            proj4string = CRS(proj4string(habitat)))
-
-# Crop bottom type polygons to the bounding box
-VPShabitat <- intersect(habitat, bounds)
-VPShabitat$VPSlength <- SpatialLinesLengths(as(VPShabitat, 'SpatialLines')) * 1000
-VPShabitat$VPSarea <- area(VPShabitat)
-VPShabitat$VPSacres <- VPShabitat$VPSarea * 0.00024711
-
-VPShabitat@data %>%
+VPShabitat <- habitat %>%
+  # Crop bottom type polygons to the bounding box of fish detections
+  st_crop(st_bbox(fish_pos)) %>%
+  # Lengths and area of cropped polygons
+  mutate(VPSlength = st_length(.),
+         VPSarea = st_area(.)) %>%
+  # Summarize by sediment type
   group_by(Group_, SubGroup) %>%
   summarize(grp.area = sum(VPSarea)) %>%
   ungroup() %>%
