7e50c03f522f6b0a813d99aec9c45c9346abd51e
mhpob <mikeob9@gmail.com>
2018-06-20 15:06:58 -0400

    Renaming of variables.

--- spatial analysis.R
+++ spatial analysis.R
@@ -3,30 +3,30 @@ library(dplyr); library(sf)
 # Data loading ----
 # Load NCBO bottom type data and pull out Marshyhope polygons
 habitat <- read_sf(dsn ='C:/Users/secor/Downloads/2015 Atlantic Sturgeon Habitat Geodatabase and Report Nanticoke and Tributaries-2016-01-19/2015 Atlantic Sturgeon Habitat Geodatabase Nanticoke and Tributaries 01132016.gdb',
-                          layer = 'RiverBed_Habitat_Polygons_CMECS_SC_01132016')
-habitat <- habitat[habitat$Location == 'Marshyhope Creek, MD',]
-
-# Reproject from UTM to longlat (epsg = 4326)
-habitat <- st_transform(habitat, 4326)
+                          layer = 'RiverBed_Habitat_Polygons_CMECS_SC_01132016') %>%
+  filter(Location == 'Marshyhope Creek, MD') %>%
+  mutate(sed_type = case_when(SubGroup == '<Null>' ~ Group_,
+                           T ~ SubGroup)) %>%
+  # Reproject from UTM to longlat (epsg = 4326)
+  st_transform(4326)
 
 
 # Load position data
-all_pos <- read.csv(
+fish_pos <- read.csv(
   'p:/obrien/biotelemetry/nanticoke/vps results/positions/all-calc-positions.csv',
                     stringsAsFactors = F) %>%
-  mutate(DATETIME = lubridate::ymd_hms(DATETIME)) %>%
-  rename_all(tolower)
-
-fish_pos <- all_pos %>%
-  filter(grepl('^\\d', all_pos$transmitter)) %>%
+  rename_all(tolower) %>%
+  filter(grepl('^\\d', transmitter)) %>%
+  mutate(datetime = lubridate::ymd_hms(datetime)) %>%
   st_as_sf(coords = c('lon', 'lat'), crs = 4326)
 
 
 # Point-in-Polygon analysis ----
 # Find which habitats detections fall into
 pip <- st_join(fish_pos, habitat, join = st_intersects) %>%
-  group_by(transmitter, Group_, SubGroup) %>%
-  summarize(n())
+  group_by(transmitter, sed_type) %>%
+  summarize(n = n()) %>%
+  mutate(prop = n/sum(n))
 
 # Area of bottom types in VPS array ----
 VPShabitat <- habitat %>%
@@ -36,7 +36,20 @@ VPShabitat <- habitat %>%
   mutate(VPSlength = st_length(.),
          VPSarea = st_area(.)) %>%
   # Summarize by sediment type
-  group_by(Group_, SubGroup) %>%
+  group_by(sed_type) %>%
   summarize(grp.area = sum(VPSarea)) %>%
   ungroup() %>%
-  mutate(prop.area = grp.area / sum(grp.area) * 100)
+  mutate(prop = as.numeric(grp.area / sum(grp.area)))
+
+
+plot.data <- full_join(data.frame(VPShabitat), data.frame(pip)) %>%
+  mutate(category = case_when(is.na(transmitter) ~ 'Habitat',
+                              T ~ transmitter))
+library(ggplot2)
+ggplot() + geom_col(data = plot.data,
+                    aes(x = sed_type,
+                        y = prop, fill = category),
+                    position = 'dodge') +
+  labs(x = NULL, y = 'Proportion in Array', fill = NULL) +
+  scale_fill_manual(values = c(colorRampPalette(c('lightgreen', 'darkgreen'))(8), 'black')) +
+  theme_bw()
