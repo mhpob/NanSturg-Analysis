57d669d60c26ba5a0b42b18a35a00425b37acfba
Mike O'Brien <mikeob9@gmail.com>
2020-09-23 17:14:26 -0400

    Log scale dendritic histograms.

--- Manuscript/dendritic histograms.R
+++ Manuscript/dendritic histograms.R
@@ -36,53 +36,105 @@ test <- rkms[detections, on = 'station', allow.cartesian= T]
 
 library(ggplot2); library(patchwork)
 dendr <- function(month){
-nan <- ggplot(data = test[!is.na(lat) &
-                            month(date.local) == month &
-                            grepl('Nan', body),]) +
-  geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
-                 breaks = seq(0, 75, 3), color = 'white') +
-  labs(x = 'River kilometer', y = 'Detection count') +
-  scale_x_continuous(limits = c(0, 75), expand = c(0, 0))
+  # create log10(x + 0.01) transformation
+  log10_p <- scales::trans_new(name = 'log10_p',
+                                transform = function(.) log10(. + 0.01),
+                                inverse = function(.) (10 ^ .) - 0.01)
 
-marsh <- ggplot(data = test[!is.na(lat) &
+  nan <- ggplot(data = test[!is.na(lat) &
                               month(date.local) == month &
-                              grepl('Marsh', body),]) +
-  geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
-                 breaks = seq(0, 30, 3), color = 'white') +
-  labs(x = NULL, y = NULL)+
-  scale_x_continuous(limits = c(0, 30), expand = c(0, 0))
+                              grepl('Nan', body),]) +
+    geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
+                   breaks = seq(0, 75, 3), color = 'white') +
+    labs(x = NULL, y = 'Detection count') +
+    coord_trans(y = log10_p) +
+    scale_y_continuous(breaks = 10 ^ (0:5),
+                       limits = c(0, 1.5e5), expand = c(0, 0),
+                       labels = c(10 ^ (0:4), 1e5)) +
+    scale_x_continuous(limits = c(0, 75), expand = c(0, 0))
+
+  marsh <- ggplot(data = test[!is.na(lat) &
+                                month(date.local) == month &
+                                grepl('Marsh', body),]) +
+    geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
+                   breaks = seq(0, 30, 3), color = 'white') +
+    labs(x = NULL, y = NULL) +
+    coord_trans(y = log10_p) +
+    scale_y_continuous(breaks = 10 ^ (0:4),
+                       limits = c(0, 6e4), expand = c(0, 0),
+                       labels = 10 ^ (0:4)) +
+    scale_x_continuous(limits = c(0, 30), expand = c(0, 0))
 
 
-broad <- ggplot(data = test[!is.na(lat) &
-                              month(date.local) == month &
-                              grepl('Broad', body),]) +
-  geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
-                 breaks = seq(0, 12, 3), color = 'white') +
-  labs(x = NULL, y = NULL)+
-  scale_x_continuous(limits = c(0, 12), expand = c(0, 0))
+  broad <- ggplot(data = test[!is.na(lat) &
+                                month(date.local) == month &
+                                grepl('Broad', body),]) +
+    geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
+                   breaks = seq(0, 12, 3), color = 'white') +
+    labs(x = NULL, y = NULL)+
+    coord_trans(y = log10_p) +
+    scale_y_continuous(breaks = 10 ^ (0:3),
+                       limits = c(0, 1e4), expand = c(0, 0),
+                       labels = 10 ^ (0:3)) +
+    scale_x_continuous(limits = c(0, 12), expand = c(0, 0))
 
 
-deep <- ggplot(data = test[!is.na(lat) &
-                             month(date.local) == month &
-                             grepl('Deep', body),]) +
-  geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
-                 breaks = seq(0, 3, 3), color = 'white') +
-  labs(x = NULL, y = NULL) +
-  scale_x_continuous(limits = c(0, 3), expand = c(0, 0))
+  deep <- ggplot(data = test[!is.na(lat) &
+                               month(date.local) == month &
+                               grepl('Deep', body),]) +
+    geom_histogram(aes(x = rkm_body_mouth, group = transmitter),
+                   breaks = seq(0, 3, 3), color = 'white') +
+    labs(x = NULL, y = NULL) +
+    coord_trans(y = log10_p) +
+    scale_y_continuous(breaks = 10 ^ (0:3),
+                       limits = c(0, 1e4), expand = c(0, 0),
+                       labels = 10 ^ (0:3)) +
+    scale_x_continuous(limits = c(0, 3), expand = c(0, 0))
 
 
 
-design <- c(
-  area(t = 1, r = 100, b = 33, l = 62),
-  area(t = 34, r = 100, b = 66, l = 0),
-  area(t = 67, r = 92, b = 99, l = 81),
-  area(t = 67, r = 100, b = 99, l = 94)
-)
+  design <- c(
+    area(t = 1, r = 100, b = 33, l = 62),
+    area(t = 34, r = 100, b = 66, l = 0),
+    area(t = 67, r = 92, b = 99, l = 81),
+    area(t = 67, r = 100, b = 99, l = 94)
+  )
 
-marsh + nan + broad + deep + plot_layout(design = design) +
-  plot_annotation(title = month.name[month],
-                  theme = theme(plot.title = element_text(size = 18))) &
-  theme_bw()
+  marsh + nan + broad + deep + plot_layout(design = design) &
+    # plot_annotation(title = month.name[month],
+    #                 theme = theme(plot.title = element_text(size = 18))) &
+    theme_bw() &
+    theme(plot.margin = margin(0, 3, 0, 0))
 }
 
+
+library(ragg)
+agg_png('manuscript/figures/dendr_hist/log_5may.png', width = 4500, height = 2550,
+        res = 600)
+dendr(5)
+dev.off()
+
+agg_png('manuscript/figures/dendr_hist/log_6jun.png', width = 4500, height = 2550,
+        res = 600)
+dendr(6)
+dev.off()
+
+agg_png('manuscript/figures/dendr_hist/log_7jul.png', width = 4500, height = 2550,
+        res = 600)
+dendr(7)
+dev.off()
+
+agg_png('manuscript/figures/dendr_hist/log_8aug.png', width = 4500, height = 2550,
+        res = 600)
+dendr(8)
+dev.off()
+
+agg_png('manuscript/figures/dendr_hist/log_9sept.png', width = 4500, height = 2550,
+        res = 600)
 dendr(9)
+dev.off()
+
+agg_png('manuscript/figures/dendr_hist/log_10oct.png', width = 4500, height = 2550,
+        res = 600)
+dendr(10)
+dev.off()
