00e4815f7c6e63b09b8092f309f4644820a13794
Mike O'Brien <mikeob9@gmail.com>
2021-04-07 14:28:58 -0400

    Include 0 in x axis and update margins.

--- rkm v watershed.R
+++ rkm v watershed.R
@@ -15,10 +15,12 @@ ggplot(data = tribs, aes(x = river.km, y = watershed, label = Abbreviation,
   scale_y_log10(limits = c(100, NA),
                 breaks = c(10^(2:6)),
                 labels = c('10\u00B2', '10\u00B3', '10\u2074', '10\u2075', '10\u2076')) +
+  scale_x_continuous(limits = c(0, NA),
+                     expand = expansion(c(0, 0.04))) +
   labs(x = 'Tributary extent (km)', y = expression(Watershed~(km^2))) +
   theme_bw() +
   theme(axis.text = element_text(size = 12),
         axis.title = element_text(size = 14),
-        plot.margin = margin(0, 0, 0, 0))
+        plot.margin = margin(0, 2, 1, 0))
 
 dev.off()
